2b725ed669242221a8525dd7a7b55a07dd896339
Leandro-dev-ops <leandrogalaxys4mini@gmail.com>
2025-05-03 11:35:18 -0300

    teste

--- src/test/java/com/oauth/jwt/authenticate/AuthenticateApplicationTests.java
+++ src/test/java/com/oauth/jwt/authenticate/AuthenticateApplicationTests.java
@@ -1,13 +1,18 @@
 package com.oauth.jwt.authenticate;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@SpringBootTest(classes = AuthenticateApplication.class)
 class AuthenticateApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String teste= "teste";
+		Assertions.assertThat(teste).isEqualTo("teste");
 	}
 
 }
